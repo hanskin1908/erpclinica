adcbbf6b410dcade8c7eaca12032e5b89b38f380
hanskin1908 <hanskch1993@hotmail.com>
2025-02-13 12:20:45 -0500

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://erpbackendhospital-production.up.railway.app/' // Reemplázalo con la URL de tu backend
+  apiUrl: 'https://erpbackendhospital-production.up.railway.app' // Reemplázalo con la URL de tu backend
 };
